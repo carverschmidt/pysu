19509828942d5ace50902b396990b9e5287378f9
Carver Schmidt <cjs5194@rit.edu>
2020-01-14 00:18:43 -0500

    highlight squares blue when selected

--- pysu.py
+++ pysu.py
@@ -13,6 +13,7 @@ import pygame
 # define colors
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
 
 class Square:
     def __init__(self, rect):
@@ -40,10 +41,27 @@ class Board(pygame.Surface):
         self.selected = None
 
     def draw(self):
+        self.fill(WHITE)
         for row in self.grid:
             for square in row:
                 pygame.draw.rect(self, BLACK, square.rect, width=1)
-                    
+                if square.val is not None:
+                    if square.temp:
+                        font = pygame.font.Font(None, 18)
+                        text = font.render(str(square.val), True, BLACK)
+                        self.blit(text, (0, 0))
+                    elif not square.temp:
+                        font = pygame.font.Font(None, 24)
+                        text = font.render(str(square.val), True, BLACK)
+                        self.blit(text, (0, 0))
+        if self.selected is not None:
+            pygame.draw.rect(self, BLUE, self.selected.rect, width=2)
+
+
+    def handle_click(self, pos):
+        col = pos[0] // (self.get_width() // 9)
+        row = pos[1] // (self.get_height() // 9)
+        self.selected = self.grid[row][col]
 
 def main():
     pygame.init()
@@ -59,13 +77,17 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                if pos[0] < board.get_width() and pos[1] < board.get_height():
+                    board.handle_click(pos)
            
         screen.fill(WHITE)
         
         board.draw()
         screen.blit(board, (0, 0))
         
-        clock.tick(15)
+        clock.tick(60)
         pygame.display.flip()
 
     pygame.quit()
